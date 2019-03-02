cffde689dc33b32fb77c04d177815584acaef65e
lionel-git <lionel.desorme@free.fr>
2019-03-02 12:59:12 +0100

    Test fields

--- CheckJsonFields/Program.cs
+++ CheckJsonFields/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +12,31 @@ namespace CheckJsonFields
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello world");
+            string jsonFile = @"c:\tmp\myData.json";
+
+            try
+            {
+                var myData = new MyData
+                {
+                    Name = "Bad Boys",
+                    Year = 1995
+                };
+
+                // serialize JSON to a string and then write string to a file
+                //File.WriteAllText(jsonFile, JsonConvert.SerializeObject(myData));
+
+                string jsonContent = File.ReadAllText(jsonFile);
+                var result = JsonConvert.DeserializeObject<MyData>(jsonContent);
+                Console.WriteLine(JsonConvert.SerializeObject(result));
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Console.WriteLine("========================");
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
